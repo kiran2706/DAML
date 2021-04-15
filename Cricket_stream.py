f66747991989116af4c191ce92b4b27a673e8d6a
Kiran R <71453237+kiran2706@users.noreply.github.com>
2021-04-15 13:47:31 +0530

    Delete Cricket_stream.py

--- Cricket_stream.py
+++
@@ -1,5 +0,0 @@
-from pycaret.classification import load_model, predict_model
-import streamlit as st
-import pandas as pd
-
-st.markdown('<style>body{background-color: white;}</style>',unsafe_allow_html=True)
