2ecf6364068039d8a2f1468ca363764262a722a3
Kiran R <71453237+kiran2706@users.noreply.github.com>
2021-04-15 13:47:05 +0530

    Update Cricket_stream.py

--- Cricket_stream.py
+++ Cricket_stream.py
@@ -2,22 +2,4 @@ from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
 
-st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=True)
-
-
-
-def predict(model, input_df):
-    predictions_df = predict_model(estimator=model, data=input_df)
-    predictions = predictions_df['Label'][0]
-    score = predictions_df['Score'][0]
-    return [predictions,score]
-
-def run():
-
-    st.title("Predicting Oneday odi match Result")
-   
-def main():
-    run()
-
-if __name__ == "__main__":
-  main()
+st.markdown('<style>body{background-color: white;}</style>',unsafe_allow_html=True)
